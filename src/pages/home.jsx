1a37e8bff654b2b917c7c7903d966b45c90b0572
Nawaf v p <nawafvp.dev@gmail.com>
2023-10-18 01:58:40 +0530

    fix : added files

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BasicExample from '../components/Topnav'
+
+const Home = () => {
+  return (
+    <>
+      <BasicExample/>
+    </>
+  )
+}
+
+export default Home;
