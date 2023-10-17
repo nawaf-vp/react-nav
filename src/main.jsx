ff0489595c3fc31f773700086a9e4f88156d5f80
Abdu1-Rahman <abduofficial01@gmail.com>
2023-10-17 22:38:50 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BasicExample from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BasicExample />
+  </React.StrictMode>,
+)
+
+
